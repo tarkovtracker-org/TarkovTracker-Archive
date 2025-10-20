a019f4a987286f6f555fb61d4692c18aec74e6c6
Dys <github@dysektai.com>
2025-10-20 00:15:25 -0400

    fix(shared): freeze shared player constants

--- packages/shared/constants/player.js
+++ packages/shared/constants/player.js
@@ -1,5 +1,5 @@
 export const MAX_PLAYER_LEVEL = 79;
 
-export const ALLOWED_PMC_FACTIONS = ['USEC', 'BEAR'];
+export const ALLOWED_PMC_FACTIONS = Object.freeze(['USEC', 'BEAR']);
 
-export const OBJECTIVE_PROGRESS_STATES = ['completed', 'uncompleted'];
+export const OBJECTIVE_PROGRESS_STATES = Object.freeze(['completed', 'uncompleted']);
